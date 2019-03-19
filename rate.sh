ddcd66bc8a8d98e3eb4e0cde8f49bb311de796be
Yosuke Kobayashi <parmalatinter@gmail.com>
2019-03-19 17:58:34 +0900

    Update rate.sh

--- rate.sh
+++ rate.sh
@@ -1,44 +1,33 @@
 #!/bin/bash
-index=$1
-
-if [ -n $index ]; then
-	$index=$0
-else
-	echo 'index?'
-	read name;
-	$index=$name
-fi
-
-# https://stedolan.github.io/jq/ jqをインストールする
-# 0 "GBPNZD"
-# 1 "CADJPY"
-# 2 "GBPAUD"
-# 3 "AUDJPY"
-# 4 "AUDNZD"
-# 5 "EURCAD"
-# 6 "EURUSD"
-# 7 "NZDJPY"
-# 8 "USDCAD"
-# 9 "EURGBP"
-# 10 "GBPUSD"
-# 11 "ZARJPY"
-# 12 "EURCHF"
-# 13 "CHFJPY"
-# 14 "AUDUSD"
-# 15 "USDCHF"
-# 16 "EURJPY"
-# 17 "GBPCHF"
-# 18 "EURNZD"
-# 19 "NZDUSD"
-# 20 "USDJPY"
-# 21 "EURAUD"
-# 22 "AUDCHF"
-# 23 "GBPJPY"
-
 
 # 受け側のURL
 url="https://www.gaitameonline.com/rateaj/getrate"
 
+if [ -n "$1"]; then
+
+	ITEM_LIST="GBPNZD CADJPY GBPAUD AUDJPY AUDNZD EURCAD EURUSD NZDJPY USDCAD EURGBP GBPUSD ZARJPY EURCHF CHFJPY AUDUSD USDCHF EURJPY GBPCHF EURNZD NZDUSD USDJPY EURAUD AUDCHF GBPJPY"
+
+	select selection in $ITEM_LIST
+	do
+	  if [ $selection ]; then
+		curl $url | jq-win64.exe ".[] | .[$(($REPLY-1))] | .bid"
+	    break
+	  else
+	    echo "invalid selection."
+	  fi
+	done
+else
+	curl $url | jq-win64.exe ".[] | .[$(($1-1))] | .bid"
+fi
+
+
+# while true; do
 # 実行
-curl $url | jq-win64.exe ".[] | .[$index] | .bid, .currencyPairCode"
+
+# curl $url | jq-win64.exe ".[] | .[$index] | .bid, .currencyPairCode"
+
+# sleep 60
+# done
+
+
 
